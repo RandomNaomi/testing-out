9511f65deacb324660eedf4f6fba829b301e64d3
RandomNaomi <94343235+RandomNaomi@users.noreply.github.com>
2024-04-23 22:32:46 -0300

    Add files via upload
    updated

---
+++ testing-out-updated-main/task/src/main/java/br/sp/gov/etesp/task/controller/HomeController.java
@@ -0,0 +1,93 @@
+package br.sp.gov.etesp.task.controller; //PROJETO TURMA B
+
+
+import java.time.LocalDate;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.sp.gov.etesp.task.model.Tarefa;
+import br.sp.gov.etesp.task.repository.TarefaRepository;
+import br.sp.gov.etesp.task.util.StatusTarefa;
+import jakarta.validation.Valid;
+
+
+@Controller
+public class HomeController {
+			
+	
+	@Autowired
+	TarefaRepository repository;
+	
+	
+	
+	
+	
+	@GetMapping("/")
+	
+	public String abrirHome(Model model) {
+		List<Tarefa> tarefas = repository.findAll();
+		model.addAttribute(new Tarefa());
+		model.addAttribute("tarefas", tarefas);
+		return "home";
+	}
+	
+	
+	@PostMapping("/adicionar")
+	public String adicionarTarefa(Tarefa tarefa, Model model){
+		
+		
+		tarefa.setStatus(StatusTarefa.ABERTO.name());
+		
+		tarefa.setDataInicio(LocalDate.now());		
+		repository.save(tarefa);
+		List<Tarefa> tarefas = repository.findAll();	
+		
+		model.addAttribute("tarefas", tarefas);
+		
+		return "home";
+		
+	}
+	
+	@GetMapping("/encerrar/{id}")
+	public String encerrarTarefa(Model model, @PathVariable Long id) {
+		
+		Tarefa tarefa = repository.findById(id).get();
+		tarefa.setStatus(StatusTarefa.FECHADO.name());
+		tarefa.setDateFim(LocalDate.now());
+		repository.save(tarefa);
+		List<Tarefa> tarefas = repository.findAll();
+		model.addAttribute("tarefas", tarefas);
+		return "home";
+		
+	}
+	
+	@GetMapping("/excluir/{id}")
+	public String excluirTarefa(Model model, @PathVariable Long id) {
+		repository.deleteById(id);
+		List<Tarefa> tarefas = repository.findAll();
+		model.addAttribute("tarefas", tarefas);
+		return "home";
+	}
+	
+	@GetMapping("/editar/{id}")
+	public String editarTarefa(Model model, @PathVariable Long id) {
+		Tarefa tarefa = repository.findById(id).get();
+		model.addAttribute("tarefa", tarefa);
+		
+		return "editar-tarefa";
+	}
+	
+	
+	
+	
+}
